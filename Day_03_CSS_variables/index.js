4218e03a05d4c8dc2632da0b4e6aaea389d12a62
Nhan Nguyen <trinhan194@gmail.com>
2021-06-10 20:09:32 +0700

    Finish Day 03

---
+++ Day_03_CSS_variables/index.js
@@ -0,0 +1,17 @@
+const inputs = document.querySelectorAll(".controls input");
+
+function handleInputChange() {
+  const suffix = this.dataset.suffix || "";
+  //   console.log(this.value + suffix);
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
+}
+
+inputs.forEach((input) => input.addEventListener("change", handleInputChange));
+
+// Below is for continously update when range is changing
+inputs.forEach((input) =>
+  input.addEventListener("mousemove", handleInputChange)
+);
