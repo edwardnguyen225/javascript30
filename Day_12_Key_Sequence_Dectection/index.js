bd364fe11c8c6262238a5a6b05410db606424bc0
Nhan Nguyen <trinhan194@gmail.com>
2021-06-20 10:45:39 +0700

    Finish Day 12

---
+++ Day_12_Key_Sequence_Dectection/index.js
@@ -0,0 +1,18 @@
+const pressedKeys = [];
+const secretCode = "hirito";
+
+window.addEventListener("keyup", (e) => {
+  const newKey = e.key;
+  console.log(newKey);
+  pressedKeys.push(newKey);
+  pressedKeys.splice(
+    -secretCode.length - 1,
+    pressedKeys.length - secretCode.length
+  );
+  console.log(pressedKeys);
+
+  if (pressedKeys.join("").includes(secretCode)) {
+    console.log("DING DING DING!!! 🎉🎉🎉");
+    console.log(`You found "${secretCode}"`);
+  }
+});
