18b130bdbf571855e5d0f1b55d9c54616186b68f
Nhan Nguyen <trinhan194@gmail.com>
2021-06-21 14:22:12 +0700

    Format js

--- Day_13_Slide_in_on_Scroll/index.js
+++ Day_13_Slide_in_on_Scroll/index.js
@@ -1,34 +1,34 @@
 
-function debounce(func, wait = 20, immediate = true) {
-    var timeout;
-    return function () {
-        var context = this, args = arguments;
-        var later = function () {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
+function debounce (func, wait = 20, immediate = true) {
+  let timeout;
+  return function () {
+    const context = this; const args = arguments;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
     };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
 }
 
 const sliderImgs = document.querySelectorAll('.slide-in');
 
-function updateSlide() {
-    sliderImgs.forEach((sliderImg => {
-        const slideInAt = window.scrollY + window.innerHeight - sliderImg.height / 4;
-        const imgBottom = sliderImg.offsetTop + sliderImg.height;
-        const isAQuaterShown = slideInAt > sliderImg.offsetTop;
-        const isNotScrollPast = window.scrollY < imgBottom;
+function updateSlide () {
+  sliderImgs.forEach(sliderImg => {
+    const slideInAt = window.scrollY + window.innerHeight - sliderImg.height / 4;
+    const imgBottom = sliderImg.offsetTop + sliderImg.height;
+    const isAQuaterShown = slideInAt > sliderImg.offsetTop;
+    const isNotScrollPast = window.scrollY < imgBottom;
 
-        if (isAQuaterShown && isNotScrollPast) {
-            sliderImg.classList.add('active');
-        } else {
-            sliderImg.classList.remove('active');
-        }
-    }));
+    if (isAQuaterShown && isNotScrollPast) {
+      sliderImg.classList.add('active');
+    } else {
+      sliderImg.classList.remove('active');
+    }
+  });
 }
 
-window.addEventListener("scroll", debounce(updateSlide))
+window.addEventListener('scroll', debounce(updateSlide));
